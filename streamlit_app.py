9370bf9f058b7db3716c5c1ee84fa81fd56aa2a8
Jianwei Hao <45022925+glennjw@users.noreply.github.com>
2023-11-13 21:09:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,20 +8,32 @@ import pickle
 #to load a saved modelimport base64  
 #to open .gif files in streamlit app
 
-@st.cache(suppress_st_warning=True)
-def get_fvalue(val):
-  feature_dict = {"No":1,"Yes":2}
-  for key,value in feature_dict.items():
-    if val == key:
-      return value
+# @st.cache(suppress_st_warning=True)
+# def get_fvalue(val):
+#   feature_dict = {"No":1,"Yes":2}
+#   for key,value in feature_dict.items():
+#     if val == key:
+#       return value
       
-def get_value(val,my_dict):
-  for key,value in my_dict.items(): 
-    if val == key:
-      return value
+# def get_value(val,my_dict):
+#   for key,value in my_dict.items(): 
+#     if val == key:
+#       return value
       
       
-app_mode = st.sidebar.selectbox('Select Page',['Home','Prediction']) #two pages
+# app_mode = st.sidebar.selectbox('Select Page',['Home','Prediction']) #two pages
 
 
 
+# Streamlit app
+st.title("LLM Lyrics Generation")
+
+# User input text area
+user_input = st.text_area("Enter a starting text:", "Once upon a time in a land far away...")
+
+# Button to generate lyrics
+if st.button("Generate Lyrics"):
+    # Use your model to generate lyrics based on user input
+    generated_lyrics = generate_lyrics(user_input)
+    st.write("Generated Lyrics:")
+    st.write(generated_lyrics)
